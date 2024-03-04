eb42bf86e215fee4ef2473690fd764dc354f9b56
yin-lo <yinlo@hotmail.fr>
2024-03-04 13:26:09 +0100

    etape 5 avant le 4, route dynamique mais pas la nav

--- index.js
+++ index.js
@@ -10,18 +10,30 @@ app.set('view engine', 'ejs')
 
 app.use(express.static('public'));
 
+const games = require('./games.json');
 
 app.get('/', (req, res) => {
 	res.render('index.ejs', {css : ''});
 });
 
 
-app.get('/game/fourchette', (req, res) => {
-    res.render('fourchette', {css : ''});
-  }); 
-app.get('/game/diceRoller', (req, res) => {
-    res.render('diceRoller', {css : '/css/diceRoller.css'});
-  }); 
+// app.get('/game/fourchette', (req, res) => {
+//     res.render('fourchette', {css : ''});
+//   }); 
+// app.get('/game/diceRoller', (req, res) => {
+//     res.render('diceRoller', {css : '/css/diceRoller.css'});
+//   }); 
+
+
+  app.get('/game/:activeGame', (req,res) => {
+	const game = games.find((game) => game.name === req.params.activeGame);
+
+	console.log(game.name);
+	
+	res.render(`${game.name}`, {css : `${game.cssFile}`});
+})
+
+
 
 app.listen(PORT, () => {
 	console.log(`Listening at http://localhost:${PORT}`);
